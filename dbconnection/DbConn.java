526b4c5a80198ca6b02d7507b66ee92b7bb7e9f6
Kavinraj2408 <kavinraj2408@gmail.com>
2024-02-20 18:18:33 +0530

    additional Changes

--- dbconnection/DbConn.java
+++ dbconnection/DbConn.java
@@ -7,15 +7,26 @@ public class DbConn {
         String url = "jdbc:mysql://localhost:3306/Sampledatabase";
         String userName = "root";
         String pw = "root";
-        String query = "SELECT name FROM users WHERE id=3"; // No need to specify the database name in the query
+        String query = "SELECT * FROM users"; 
+        String insertQuery = "insert into users (id,name) values (4,'Harsith')";
+        String insertQuery1 = "insert into users (id,name) values (?,?)";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, userName, pw);
+            PreparedStatement ps = conn.prepareStatement(insertQuery1);
+            ps.setInt(1, 5);
+            ps.setString(2, "Buvi");
+            //insert query
+            int count = ps.executeUpdate();
             Statement st = conn.createStatement();
+            //insert query
+            int row = st.executeUpdate(insertQuery);
+            //select query
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                String name = rs.getString("name");
-                System.out.println("Name: " + name);
+            	int id =  rs.getInt("id");
+;                String name = rs.getString("name");
+                System.out.println("Id: "+ id +" Name: " + name);
             }
             st.close();
             conn.close(); // Close the connection when done
